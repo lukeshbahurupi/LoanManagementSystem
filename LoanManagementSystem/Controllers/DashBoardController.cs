c1c55ba4e3d20f44bfb9a853b801c2996596b8f8
Lukesh Bahurupi <lbahurupi567@gmail.com>
2023-08-22 22:10:41 +0530

    Application form validation pending

---
+++ LoanManagementSystem/Controllers/DashBoardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoanManagementSystem.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ApplicationForm()
+        {
+            return View();
+        }
+    }
+}
